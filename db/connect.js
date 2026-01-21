aa440c07a1557c50421b47bb28c6adafe8e86ad0
janvi-png <sharmajjjanvi@gmail.com>
2026-01-21 22:25:22 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -11,15 +11,15 @@ async function connectDB() {
 
   client = new MongoClient(uri, {
     tls: true,
-    tlsAllowInvalidCertificates: true, // REQUIRED on Render
-    serverSelectionTimeoutMS: 15000,
-    directConnection: true, // IMPORTANT for Railway proxy
+    tlsInsecure: true,
+    directConnection: true,
+    serverSelectionTimeoutMS: 20000,
   });
 
   await client.connect();
   db = client.db("collabDB");
 
-  console.log("✅ Connected to MongoDB (Railway from Render)");
+  console.log("✅ MongoDB connected (Render → Railway)");
   return db;
 }
 
